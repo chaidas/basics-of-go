6fe5a7074e2719d91ed72bbfe72ad279a6194f43
Angelos Chaidas <achaidas@gmail.com>
2024-11-14 09:24:33 +0200

    Playing around with collections (tidied up) ... and fix problems!

--- go-input-output/data/collections.go
+++ go-input-output/data/collections.go
@@ -2,32 +2,23 @@ package data
 
 import "fmt"
 
-// Countries Array example
-var Countries [10]string
-
-// Slice An example of a slice
-var Slice []int
-
-// Codes Map example
-var Codes map[int]string
-
 // init() method is executed typically when you start your app
 // A function where you can initialise things
 // You can have more than one init() function in the same package in the same file!
 // (Careful: init() is _not_ main)
 func init() {
-	Countries[0] = "Argentina"
-	Countries[1] = "Brazil"
-	Countries[8] = "USA"
+
+	var countries [10]string
+	countries[0] = "Argentina"
+	countries[1] = "Brazil"
+	countries[8] = "USA"
 
 	// Getting the length of arrays
-	qty := len(Countries)
-
-	fmt.Println("Length of Countries array", qty)
+	fmt.Println("Length of Countries array", len(countries))
 
 	// Slice literal example
-	countries := []string{"France", "Japan", "Brazil"}
-	fmt.Println(countries)
+	countriesSlice := []string{"France", "Japan", "Brazil"}
+	fmt.Println(countriesSlice)
 
 	// Map literal example
 	wellKnownPorts := map[string]int{
